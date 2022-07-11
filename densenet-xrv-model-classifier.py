e7ec2f0ab84281a9c60a0c178997d7ea67ce51d4
Rino Santoso <43334126+rinoSantoso@users.noreply.github.com>
2022-07-11 17:06:41 +0700

    Update densenet-xrv-model-classifier.py

--- densenet-xrv-model-classifier.py
+++ densenet-xrv-model-classifier.py
@@ -336,5 +336,32 @@ print("AUC score: ")
 print(auc.compute())
 
 
+dataset_classes = ['Clean','Dirty']
 
+def imshow(imgnumpy: np.ndarray, label, denormalize=False):
+    plt.imshow(tensor_to_imgnumpy_simple(imgnumpy))
+    plt.title(dataset_classes[label])
+    
+loader = DataLoader(model.dataset_test, batch_size=1, shuffle=True)
+
+plt.figure(figsize=(20, 8))
+for idx,(img,label) in enumerate(loader):
+    plt.subplot(4,10,idx+1)
+    imshow(img[0],label,denormalize=True)
+    
+    # inference
+    try:
+        pred = model.forward(img.cuda())
+    except Exception as e:
+        pred =  model.forward(img)
+        print(e)
+
+    title_dataset = dataset_classes[label]
+    title_pred = dataset_classes[pred.argmax()]
+    plt.title(f"{title_dataset}({title_pred})",color=("green" if title_dataset==title_pred else "red"))
+    
+    if idx == 40-1:
+        break
+        
+plt.tight_layout()
 
