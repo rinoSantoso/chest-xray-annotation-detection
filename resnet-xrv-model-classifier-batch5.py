d28e0ee92f0629a63a5af225b010775120ae7a84
Rino Santoso <43334126+rinoSantoso@users.noreply.github.com>
2022-07-17 21:52:20 +0700

    min delta reduced

--- resnet-xrv-model-classifier-batch5.py
+++ resnet-xrv-model-classifier-batch5.py
@@ -337,7 +337,7 @@ model = FinetunedModel()
 
 # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
 try:
-    trainer = pl.Trainer(gpus=1,max_epochs=100,default_root_dir='./batch5_logs_resnet', callbacks=[EarlyStopping(monitor="val_loss", mode="min", min_delta=0.01)])
+    trainer = pl.Trainer(gpus=1,max_epochs=100,default_root_dir='./batch5_logs_resnet', callbacks=[EarlyStopping(monitor="val_loss", mode="min", min_delta=0.005)])
 except Exception as e:
     # most likely due to GPU, so fallback to non GPU
     print(e)
