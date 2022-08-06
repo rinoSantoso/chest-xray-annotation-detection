18671abc3f40eca776f90f83a01c293bcc69ff51
Rino Santoso <43334126+rinoSantoso@users.noreply.github.com>
2022-08-06 23:26:41 +0700

    Update densenet-xrv-model-classifier-collection3.py

--- densenet-xrv-model-classifier-collection3.py
+++ densenet-xrv-model-classifier-collection3.py
@@ -420,7 +420,7 @@ loader = DataLoader(model.dataset_test, batch_size=1, shuffle=True)
 
 plt.figure(figsize=(40, 32))
 for idx,(img,label) in enumerate(loader):
-    plt.subplot(10,5,idx+1)
+    plt.subplot(10,10,idx+1)
     imshow(img[0],label,denormalize=True)
     
     
